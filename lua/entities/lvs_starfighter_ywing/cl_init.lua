c7adfe20175b5ebddd5d54d9925426d8d5959f4b
KurtJQ <irockstar12230@gmail.com>
2023-02-07 01:28:19 +0800

    added engine effects

--- lua/entities/lvs_starfighter_ywing/cl_init.lua
+++ lua/entities/lvs_starfighter_ywing/cl_init.lua
@@ -12,6 +12,47 @@ function ENT:OnSpawn()
     self:RegisterTrail( Vector(-637.74, 295.15, 60.08), 0, 20, 2, 1000, 150)
 end
 
+function ENT:OnFrame()
+    self:EngineEffects()
+end
+
+function ENT:EngineEffects()
+    if not self:GetEngineActive() then return end
+
+    local T = CurTime()
+
+    if (self.nextEFX or 0) > T then return end
+
+    self.nextEFX = T + 0.1
+
+    local THR = self:GetThrottle()
+
+    local emitter = self:GetParticleEmitter( self:GetPos() )
+
+    if not IsValid(emitter) then return end
+
+    for _, pos in pairs(self.EnginePos) do
+        local vOffset = self:LocalToWorld(pos)
+        local vNormal = -self:GetForward()
+
+        vOffset = vOffset + vNormal * 55
+
+        local particle = emitter:Add( "effects/muzzleflash2", vOffset )
+
+		if not particle then continue end
+
+		particle:SetVelocity( vNormal * (math.Rand(500,1000) + self:GetBoost() * 10) + self:GetVelocity() )
+		particle:SetLifeTime( 0 )
+		particle:SetDieTime( 0.1 )
+		particle:SetStartAlpha( 255 )
+		particle:SetEndAlpha( 0 )
+		particle:SetStartSize( math.Rand(50,100) )
+		particle:SetEndSize( math.Rand(10,25) )
+		particle:SetRoll( math.Rand(-1,1) * 100 )
+		particle:SetColor( 255, 50, 0 )
+	end
+end
+
 function ENT:PostDraw()
     if not self:GetEngineActive() then return end
 
