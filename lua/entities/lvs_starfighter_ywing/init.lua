98ea3357390783f7dfa4ab6adff491ecca933512
KurtJQ <irockstar12230@gmail.com>
2023-02-09 03:27:36 +0800

    started gunner seat

--- lua/entities/lvs_starfighter_ywing/init.lua
+++ lua/entities/lvs_starfighter_ywing/init.lua
@@ -9,6 +9,10 @@ function ENT:OnSpawn( PObj )
 
     self:AddDriverSeat( Vector(305 ,0 , 60), Angle(0,-90 ,0) )
 
+    local Pod = self:AddPassengerSeat( Vector(255.72, 0, 85), Angle(0, -90, 0))
+
+    self:SetTailGunnerSeat( Pod )
+
     self:AddEngine( Vector(-580,239.94,59.39) )
     self:AddEngine( Vector(-580,-239.94,59.39) )
     self:AddEngineSound( Vector(-620.53,239.94,59.39) )
@@ -16,6 +20,9 @@ function ENT:OnSpawn( PObj )
 
     self.PrimarySND = self:AddSoundEmitter( Vector(513.2, 0, 45.66), "vanilla/ywing/vanilla_ywing_wep2.wav", "vanilla/ywing/vanilla_ywing_wep2.wav")
     self.PrimarySND:SetSoundLevel(110)
+
+    self.GunnerSND = self:AddSoundEmitter( Vector(255.72, 0, 99.32), "vanilla/ywing/fire_gunner.mp3", "vanilla/ywing/fire_gunner.mp3")
+    self.GunnerSND:SetSoundLevel(110)
 end
 
 function ENT:OnEngineActiveChanged( Active )
