6ea3d9dad94ba1b8a823e2715083eb3b79253e74
KurtJQ <irockstar12230@gmail.com>
2023-02-15 22:56:32 +0800

    updated mass and camera distance

--- lua/entities/lvs_starfighter_ywing/init.lua
+++ lua/entities/lvs_starfighter_ywing/init.lua
@@ -6,10 +6,10 @@ include("shared.lua")
 ENT.SpawnNormalOffset = 100
 
 function ENT:OnSpawn( PObj )
-    PObj:SetMass( 1000 )
+    PObj:SetMass( 2500 )
 
     local DriverSeat = self:AddDriverSeat( Vector(305 ,0 , 60), Angle(0,-90 ,0) )
-    DriverSeat:SetCameraDistance(1)
+    DriverSeat:SetCameraDistance(1.5)
 
     local Pod = self:AddPassengerSeat( Vector(256, 0, 88), Angle(0, 90, 0))
     Pod.HidePlayer = true
